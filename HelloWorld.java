cf21d12419f397e117470aafc578f6fc4053430c
sh8941 <zshamoonhaider@gmail.com>
2023-06-24 17:02:07 +0530

    fist java program

---
+++ HelloWorld.java
@@ -0,0 +1,8 @@
+// public class is required in java to write main method
+public class HelloWorld{
+    // main method is starting point of a java program
+    public static void main(String[] args) {
+        // this method simply print in output that string which pass as a argument
+        System.out.println("Hello World");
+    }
+}
